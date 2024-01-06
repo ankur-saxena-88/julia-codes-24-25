3ed7c49be3568ca12aecc0cdc60b786245d24a58
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-01-06 12:40:15 +0530

    Add displayFunDemo.jl file

---
+++ displayFunDemo.jl
@@ -0,0 +1,6 @@
+# displayFunDemo.jl
+
+# Displays formatted output, especially for rich text or data structures.
+# Syntax: display("string to display")
+
+display("This is displayed using display()")

b9be1c2d38265395ccddeb604e398d204424857a
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-02-12 09:41:40 +0530

    Upload FuncAdd.jl source code file

---
+++ FuncAdd.jl
@@ -0,0 +1,9 @@
+# FuncAdd.jl
+
+function add(num1::Int, num2::Int)::Int
+    return num1 + num2
+  end
+  
+  result = add(3, 5)
+  #print("Sum ::", result)
+  println("Sum of 3 and 5 is :: ", result)

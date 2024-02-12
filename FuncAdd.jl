637adaae3faece57718da1eeb98903a6f0a9cfc7
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2024-02-12 09:44:43 +0530

    Edit line 12

--- FuncAdd.jl
+++ FuncAdd.jl
@@ -1,9 +1,12 @@
 # FuncAdd.jl
 
+# creating 'add' function
 function add(num1::Int, num2::Int)::Int
     return num1 + num2
   end
-  
+
+# calling add function
   result = add(3, 5)
+# print result
   #print("Sum ::", result)
   println("Sum of 3 and 5 is :: ", result)

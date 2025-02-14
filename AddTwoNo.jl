aa43104f0a2ee4b11267beede74f259cb482f809
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2025-02-14 12:17:07 +0530

    Add AddTwoNo.jl file

---
+++ AddTwoNo.jl
@@ -0,0 +1,19 @@
+# AddTwoNo.jl
+# Friday, 14-Feb-2025
+
+# Julia program to add two numbers using function
+
+function addTwoNo()
+
+    num1 = 20
+    num2 = 30
+
+    sum = num1 + num2
+
+    println("First number is $num1")
+    println("Second number is $num2")
+    println("The sum of $num1 and $num2 is $sum")
+end
+
+# call function
+println(addTwoNo())
